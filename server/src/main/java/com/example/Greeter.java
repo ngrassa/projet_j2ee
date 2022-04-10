2678119a0f3e1413baace87f4a84458e551d72c9
Bilel Issaoui <7354810-bileli@users.noreply.gitlab.com>
2022-04-10 10:57:16 +0000

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -9,10 +9,10 @@ public class Greeter {
    * This is a constructor.
    */
   public Greeter() {
-// Do nothing because of X and Y.
+
   }
 
-
+  //TODO: Add javadoc comment
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
